f486998504ef89993a377b26f79441b31fb59b9f
anvar <majidovanvar30.11.2002@gmail.com>
2023-04-08 18:54:13 +0500

    Added exception middleware for handling http and internal server exceptions.

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Text.Json;
+using API.Shared;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+    {
+        _logger = logger;
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+            
+            var statusCode = HttpStatusCode.InternalServerError;
+            if (ex is HttpRequestException exception) statusCode = exception.StatusCode ?? statusCode;
+            
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int) statusCode;
+            
+            var response = new ApiException 
+            {
+                StatusCode = statusCode,
+                Message = ex.Message
+            };
+            
+            var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        
+            var json = JsonSerializer.Serialize(response, options);
+        
+            await context.Response.WriteAsync(json);
+        }
+    }
+}
---
+++ API/Shared/ApiException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace API.Shared;
+
+public class ApiException
+{
+    public string Message { get; set; }
+    public HttpStatusCode StatusCode { get; set; }
+}
